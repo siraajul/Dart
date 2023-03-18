0c02c322a7ad01a366608cc9e04a659a39ef71ef
siraajul <shirajulislamparvez@gmail.com>
2023-03-19 03:00:57 +0600

    Arithmetic Operator

---
+++ Operator/arithmetic.dart
@@ -0,0 +1,24 @@
+void main(){
+  int num1=10;
+  int num2=3;
+
+  //Addition
+  var addition=num1+num2;
+  //Subtract
+  var subtract=num1-num2;
+  //Multiply
+  var multiply=num1*num2;
+  //Divide
+  var divide=num1/num2;
+  //Divide,returning an integer result
+  var divideinteger=num1~/num2;
+  //Remainder of integer division
+  var remainder=num1%num2;
+
+  print(addition);
+  print(subtract);
+  print(multiply);
+  print(divide);
+  print(divideinteger);
+  print(remainder);
+}
