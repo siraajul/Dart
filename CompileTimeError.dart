e1958d4c965cadbd6d9de9dbd1d564162a728e1b
siraajul <shirajulislamparvez@gmail.com>
2023-03-19 18:02:37 +0600

    Compile Time Error !

---
+++ CompileTimeError.dart
@@ -0,0 +1,9 @@
+//Any Kind of Syntax Error are Compile time error
+void main(){
+  int a = 10;
+  int b = 5;
+  String name = 'Bengal Droid';
+  print(name);
+  // If i remove semicolon(;) from any of the line it will not run and not give expected output
+  // That's why it is compile time error
+}
