da7ae0554e6624b85809c2427e9c81ac201947d9
siraajul <shirajulislamparvez@gmail.com>
2023-03-19 15:39:14 +0600

    if...else

---
+++ Conditional Statement/ifelse.dart
@@ -0,0 +1,13 @@
+void main(){
+  String connection = 'waiting';
+
+  if(connection=='connected'){
+    print("connected");
+  }
+  else if(connection=='waiting'){
+    print("waiting");
+  }
+  else{
+    print("disconnected");
+  }
+}
