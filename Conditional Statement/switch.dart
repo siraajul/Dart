69cc1191a08c389e0121fa148204ebf1ed4bff49
siraajul <shirajulislamparvez@gmail.com>
2023-03-19 15:50:09 +0600

    String Concatenation and Interpolation and List Join

--- Conditional Statement/switch.dart
+++ Conditional Statement/switch.dart
@@ -2,5 +2,15 @@ void main(){
 
   String connection = 'waiting';
 
+  switch(connection){
+    case 'connected':
+      print("Connected");
+      break;
+    case 'waiting':
+      print("Waiting");
+      break;
+    default:
+      print('disconnected');
+  }
 
 }
---
+++ String/concatenation.dart
@@ -0,0 +1,22 @@
+void main(){
+  String a = 'We';
+  String b = 'are';
+  String c = 'learning';
+  String d = 'dart';
+
+  //String Literals
+  String e = 'We' 'are' 'learning' 'dart';
+
+  //Concatenation
+  print(a+b+c+d);
+
+  //Interpolation
+  print("$a$b$c$d");
+
+  //String Literals
+  print(e);
+  
+  //List
+  List<String> f =['We' 'are' 'learning' 'dart'];
+  print(f.join());
+}
