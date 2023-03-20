e4fefe633decc95178c63e33b34bac563b84b2f4
siraajul <shirajulislamparvez@gmail.com>
2023-03-20 17:23:34 +0600

    Optional Positional Parameter

---
+++ Function/optional positional parameter.dart
@@ -0,0 +1,12 @@
+void main(){
+  myFunction(a,b,c,[d,e]){
+    print(a);
+    print(b);
+    print(c);
+    print(d);
+    print(e);
+  }
+  myFunction(20, 30, 40, 50);
+}
+
+//Optional Named Parameter
