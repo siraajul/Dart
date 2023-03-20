9b93585dd0e53a794499f313edcb51d8cc0de1ae
siraajul <shirajulislamparvez@gmail.com>
2023-03-20 14:45:55 +0600

    How to define function

---
+++ Function/arrow function.dart
@@ -0,0 +1,4 @@
+void main(){
+  //small function
+  arrowFunction
+}
--- Function/function.dart
+++ Function/function.dart
@@ -1,6 +1,8 @@
 void main(){
+  //define function
   addTwoNumbers(){
     print(10+20);
   }
+  //calling function
   addTwoNumbers();
 }
