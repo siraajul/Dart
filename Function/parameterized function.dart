2111679d39d7af721811512a7741d3ebf1263d1d
siraajul <shirajulislamparvez@gmail.com>
2023-03-20 17:13:38 +0600

    Parameterized Function

---
+++ Function/parameterized function.dart
@@ -0,0 +1,9 @@
+void main(){
+  addTwoNumber(int num1, int num2){
+    print(num1+num2);
+  }
+  addTwoNumber(20,30);
+  addTwoNumber(40,30);
+}
+
+//we can use multiple times in parameterized function
