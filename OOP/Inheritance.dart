03f5ebbe84bd9fe3cf7087ab38106e8266212d5e
siraajul <shirajulislamparvez@gmail.com>
2023-03-21 07:35:38 +0600

    Inheritance

---
+++ OOP/Inheritance.dart
@@ -0,0 +1,19 @@
+void main(){
+  var son = Son();
+  son.methodOne();
+  son.name;
+}
+class Father{
+  int age =50;
+  methodOne(){
+   print("This is Method 1");
+  }
+
+  methodTwo(){
+    print("This is Method 2");
+  }
+}
+class Son extends Father{
+  String name = 'son';
+
+}
