805acdfb8ce56470f64f188f0b7fc6d04153aa9c
siraajul <shirajulislamparvez@gmail.com>
2023-03-21 07:27:51 +0600

    Constructor

---
+++ OOP/Constructor.dart
@@ -0,0 +1,11 @@
+void main(){
+  var obj = Example('learning approach');
+
+}
+class Example{
+  String name;
+  Example(this.name){
+    print('this is my default constructor');
+    print(name);
+  }
+}
