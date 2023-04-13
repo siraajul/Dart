bf2552b99c2dece0e9206eda15e0cfb31c28ab9c
siraajul <shirajulislamparvez@gmail.com>
2023-04-13 22:57:04 +0600

    Premitive Data Type

---
+++ Module_01/LiveClass/data_types.dart
@@ -0,0 +1,18 @@
+//Data Types
+/*
+Number - Decimal/floating
+Decimal - 234568906
+Floating - 33.43456
+ */
+main(){
+  int accountHolderAge = 40;     //integer/int
+  double totalAmount = 40000.45;  //use 'double' to store floating type datatype
+  String accountHolderName = 'Sirajul';//use 'string' to store text in program
+  bool isAccountHolderBangladeshi = true;// use 'bool' to check true or false
+
+  print(accountHolderName);
+  print(accountHolderAge); //Account Holder Age Output
+  print(totalAmount); //Account Holder Total Amount in Bank
+  print(isAccountHolderBangladeshi);
+
+}
