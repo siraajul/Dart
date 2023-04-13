54c60e87f63d1a484231449e52c842b6a4ba2ef2
siraajul <shirajulislamparvez@gmail.com>
2023-04-14 00:17:32 +0600

    Premitive Datatype Explore

--- Module_01/LiveClass/data_types.dart
+++ Module_01/LiveClass/data_types.dart
@@ -11,6 +11,26 @@ main(){
   bool isAccountHolderBangladeshi = true;// use 'bool' to check true or false
   String address = "Brahmanbaria";
   String companyName = '''BengalDroid''';
+  String phoneNumber = '01720010907';
+
+  //List
+  List<String> phoneNumberList = [
+    '+8801318402075',
+    '+8801318402075',
+    '+8801318402075',
+    '+8801318402075',
+    '+8801318402075',
+    '+8801318402075',
+    '+8801318402075',
+    '+8801318402075'
+  ];
+
+  Map<int,String> students = {
+    1:"Rahim",
+    2:"Karim",
+    3:"Tanmay",
+    4:"Roy"
+  };
 
   String cont = accountHolderName +" "+ address;//String Concatenation
 
