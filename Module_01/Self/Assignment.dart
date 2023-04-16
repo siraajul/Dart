ae2c89b4903de12ece981116970638484c90603b
siraajul <shirajulislamparvez@gmail.com>
2023-04-16 20:36:30 +0600

    Module 01 Assignment

---
+++ Module_01/Self/Assignment.dart
@@ -0,0 +1,14 @@
+void main(){
+  List<String> phoneNumber = [
+    '+88',
+    '01768131685',
+    '01768171985',
+    '01768111286',
+    '01768131685'
+  ];
+
+  print(phoneNumber[0]+phoneNumber[1]);
+  print(phoneNumber[0]+phoneNumber[2]);
+  print(phoneNumber[0]+phoneNumber[3]);
+  print(phoneNumber[0]+phoneNumber[4]);
+}
