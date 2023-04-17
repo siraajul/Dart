2c4e248d4c2e1608a00526259dcaac44400ce63d
siraajul <shirajulislamparvez@gmail.com>
2023-04-17 23:48:54 +0600

    Live Test

---
+++ Module_01/Self/LiveTest.dart
@@ -0,0 +1,11 @@
+void main(){
+  Map<String, dynamic> employee = {
+    'Id':11610856,
+    'Name':'Sirajul Islam',
+    'Age':26,
+    'Salary':30000,
+    'Gender':'Male'
+  };
+  print(employee['Salary']);
+}
+
