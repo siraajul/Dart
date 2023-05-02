3fb6324d9488f721d9774ac82b4c03d860609d6f
siraajul <shirajulislamparvez@gmail.com>
2023-05-02 23:18:06 +0600

    Dart Control Flow

--- Module_02/Live Class/map.dart
+++ Module_02/Live Class/map.dart
@@ -1,10 +1,29 @@
 void main(){
+
   Map<String,String> user = {
     'FirstName' : 'Sirajul',
     'LastName' : 'Islam',
     'Age' : '26'
   };
 
+  Map<int,String> students = {
+    1: 'Rahim',
+    2: 'Karim',
+    3: 'Yousuf',
+    40: 'Nahid'
+  };
+
+  Map<String, dynamic> cgpaInfo = {
+    'Sirajul' : 7.27,
+    'Ebrahim' : 2.93,
+    'Sudipta' : 6.27
+  };
+
   print(user);
   print(user['Age']);
+
+  print(students);
+  print(students[40]);
+
+  print(cgpaInfo);
 }
