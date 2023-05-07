643231baf1e3b46b8777adfd8c43f54724c09780
siraajul <shirajulislamparvez@gmail.com>
2023-05-07 19:24:45 +0600

    Dart Map

---
+++ Module_02/Pre Record/map.dart
@@ -0,0 +1,10 @@
+void main(){
+  var person={
+    'name':'Sirajul',
+    'age':26,
+    'city':'Brahmanbaria'
+  };
+  //Add New Key Value to the Map
+  person['Country']='Bangladesh';
+  print(person);
+}
