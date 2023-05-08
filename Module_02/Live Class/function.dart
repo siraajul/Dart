a7fed297c1b803040a549f739602d1bd454bd853
siraajul <shirajulislamparvez@gmail.com>
2023-05-08 12:04:37 +0600

    Dart Functions

---
+++ Module_02/Live Class/function.dart
@@ -0,0 +1,16 @@
+//Have to add every where
+//Update Redundant
+//Deletion
+//Reuse-ability
+void main(){
+  //Calling Function
+  Welcome();
+}
+
+//name(Parenthesis),{Curly Braces}
+//Declaring Function
+Welcome(){
+  print("Hello Rafat");
+  print("Have a Seat");
+  print(500*40);
+}
