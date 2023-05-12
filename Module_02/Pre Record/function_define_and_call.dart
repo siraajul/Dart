6cbeeab054742de4181f0bed8560a053ed76e10d
siraajul <shirajulislamparvez@gmail.com>
2023-05-12 22:30:17 +0600

    Function

---
+++ Module_02/Pre Record/function_define_and_call.dart
@@ -0,0 +1,12 @@
+
+//define_function
+addTwoNumber(){
+  var x=1;
+  var y=3;
+  print(x+y);
+}
+
+//Function Call
+void main(){
+  addTwoNumber();
+}
---
+++ Module_03/Live Class/classes.dart
@@ -0,0 +1,16 @@
+class Human{
+  //Properties/attributes
+  late int legs;
+  late int hands;
+  late String color;
+  late int eyes;
+
+  //Behaviours/functions
+  //methods
+  void moving(){
+    print('I am Moving');
+  }
+  void eating(){
+    print;
+  }
+}
