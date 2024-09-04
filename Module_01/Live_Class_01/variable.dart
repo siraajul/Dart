b5c217d559194d126aec229f2861d0f7e0cb183b
siraajul <shirajulislamparvez@gmail.com>
2024-09-04 15:42:23 +0600

    Program -> Variables

---
+++ Module_01/Live_Class_01/variable.dart
@@ -0,0 +1,18 @@
+/*
+-> Container
+->Variable -> Container -> Contains the Value
+->Declare Syntax => data_type variable name
+*/
+main(){
+  int age = 35;
+  print(age);
+
+  double gpa = 7.27;
+  print(gpa);
+
+  bool name = true;
+  print(name);
+
+  String address = 'Brahmanbaria';
+  print(address);
+}
