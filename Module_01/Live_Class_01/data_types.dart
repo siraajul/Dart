970fee5653d348074ad5505ba7c6ec710cf6801b
siraajul <shirajulislamparvez@gmail.com>
2024-09-04 15:40:39 +0600

    Program -> DataType

---
+++ Module_01/Live_Class_01/data_types.dart
@@ -0,0 +1,14 @@
+//Data Types
+/*
+Integer -> 1,2,0,1234,567965949449
+Float ->10.0, 21.0, 2.454534534
+Boolean -> TRUE || FALSE
+Text -> name, address
+*/
+main(){
+  print(1234567890);
+  print("The Quick Brown Fox Jumps Over the Lazy Dog.");
+  print(31.5);
+  print(true);
+  print(false);
+}
