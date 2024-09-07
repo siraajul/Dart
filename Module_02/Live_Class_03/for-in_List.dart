03c1768a8856a1a32d83fe910e1f314079885a99
siraajul <shirajulislamparvez@gmail.com>
2024-09-07 22:42:23 +0600

    --Loop in For-In List

---
+++ Module_02/Live_Class_03/for-in_List.dart
@@ -0,0 +1,15 @@
+main(){
+  List<String> friends = [
+    'Rafi',
+    'Shafi',
+    'Fahad',
+    'Abdullah',
+    'Ebrahim',
+  ];
+  // for(int i=0;i<friends.length;i++){
+  //   print("${i}th friend's name is ${friends[i]}");
+  // }
+  for (String f in friends){
+    print(f);
+  }
+}
