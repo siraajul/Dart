f831b2b6944b13022b9075e5945c2d4649236652
siraajul <shirajulislamparvez@gmail.com>
2024-09-07 22:43:27 +0600

    Conditionals

---
+++ Module_02/Live_Class_02/if-else.dart
@@ -0,0 +1,14 @@
+main() {
+  ///if else
+  // jodi -> if, nahoi -> else
+  double weather = 29;
+  //jodi weather <25(kom thake), tahole ami ghurte jabo
+  if (weather < 25) {
+    //what to do
+    print('Going For Work');
+  } else if (weather >= 25 && weather <= 30) {
+    print('Will Go To Gym');
+  } else {
+    print('Watch Netflix');
+  }
+}
