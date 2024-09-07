5ace5de64a0315bf840e3438a426af1d805c9133
siraajul <shirajulislamparvez@gmail.com>
2024-09-07 22:42:39 +0600

    For Loop

---
+++ Module_02/Live_Class_02/for-loop.dart
@@ -0,0 +1,16 @@
+main() {
+  //koto bar jete hobe => n times => 5/10/50
+  //ekbar kaj korar pore check kori kotobar baki ache
+  //countig kori koto bar hoise
+
+  //count =>10(koto bar jete hobe)
+  //increment => 1+ (1 bar kore barbe)
+  //current counting < count -> r ekbar korben
+
+  //For Loop
+  //for(initialization : condition : increment/decrement){block of code}
+
+  for (int start = 0; start < 100; start++) {
+    print('Go To Shop');
+  }
+}
