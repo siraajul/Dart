573823c6640587f8c9f6d9645de17c25601829ed
siraajul <shirajulislamparvez@gmail.com>
2024-09-07 22:43:16 +0600

    Basics of function

---
+++ Module_02/Live_Class_03/function.dart
@@ -0,0 +1,21 @@
+void main() {
+  //Function Call
+  welcomeMessage('Parvez');
+  addTwoNumber(10, 20);//Arguments
+}
+
+/*Function
+Return Type function name(....parameters){
+function block/body
+}
+
+*/
+
+//Function Declaration
+welcomeMessage(String name) {
+  print('Kemon Achen ${name}');
+}
+
+addTwoNumber(int a, int b)/*Parameter*/{
+print(a+b);
+}
