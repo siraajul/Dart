b3d2b6da19b6f557f6c33d0b26207fdf4102dbb4
siraajul <shirajulislamparvez@gmail.com>
2024-09-07 22:44:00 +0600

    Set

---
+++ Module_02/Live_Class_01/set.dart
@@ -0,0 +1,12 @@
+main(){
+  //Stores only Unique value, removes duplicate values
+  //Set<String> names = {};
+  Set<String> names = {''};
+
+  //Can't Get using []
+  //Need to use 'elementsAt'
+
+  //can add list inside it
+
+  //1:10:00
+}
