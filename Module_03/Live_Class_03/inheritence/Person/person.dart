f783caa8858ee9b04f757884b054f34ced4c74c3
siraajul <shirajulislamparvez@gmail.com>
2024-09-11 23:25:05 +0600

    Inheritance --> Base class

---
+++ Module_03/Live_Class_03/inheritence/Person/person.dart
@@ -0,0 +1,21 @@
+//Base Class - Parent Class - Super class
+class Person {
+  String university = 'Dhaka University';
+  String _school = 'NDC';
+
+  String getPersonDetails() {
+    return 'Person Details';
+  }
+
+  void moving() {
+    print('person is moving');
+  }
+
+  void eating() {
+    print('Person is eating');
+  }
+
+  void _attendingClass() {
+    print('Attending Class');
+  }
+}
