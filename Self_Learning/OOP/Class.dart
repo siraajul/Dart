69a17682ebb5e4ffa06554f52c33457380ece180
siraajul <shirajulislamparvez@gmail.com>
2024-09-12 23:48:08 +0600

    Self Learning Dart

--- Self_Learning/OOP/Class.dart
+++ Self_Learning/OOP/Class.dart
@@ -1,14 +1,19 @@
 void main() {
-  Person firstPerson = Person();
-  firstPerson.displayInfo();
+  Person person = Person();
+  person.name = 'Sirajul Islam';
+  person.age = 27;
+  person.fatherName = 'Golam Hakkani';
+  person.displayInfo();
 }
 
 class Person {
-  String name = 'Asif Taj';
-  String father = 'Taj Gul';
-  int age = 25;
+  String? name;
+  String? fatherName;
+  double? age;
 
   void displayInfo() {
-    print('My Name is $name');
+    print('My Name is ${name}');
+    print('My Father Name is ${fatherName}');
+    print('My age is ${age}');
   }
 }
