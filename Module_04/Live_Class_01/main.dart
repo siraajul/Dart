c1738daafdef1c83495dd2f6135442ccf2c1e10f
siraajul <shirajulislamparvez@gmail.com>
2024-09-15 17:12:07 +0600

    Main file for checking the result

---
+++ Module_04/Live_Class_01/main.dart
@@ -0,0 +1,15 @@
+import 'interface.dart';
+import 'abstrac_class.dart';
+
+//Abstraction vs Interface vs Implementation
+void main(){
+  Student rafi =Student();
+  rafi.goingToAttendClass();//Interface
+
+  Father Nurul = Father();
+  Nurul.dressUp();
+
+  Mother Hasi = Mother();
+  Hasi.dressUp();
+
+}
