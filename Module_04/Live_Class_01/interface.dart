670d3ea98adf7ab2fc9f1ce035cd225eff57c177
siraajul <shirajulislamparvez@gmail.com>
2024-09-15 17:11:40 +0600

    Interface

---
+++ Module_04/Live_Class_01/interface.dart
@@ -0,0 +1,15 @@
+//Implementation
+class Student{
+  void goingToAttendClass(){
+    _goingToBusStand();
+    _movingViaBus();
+  }
+
+  void _goingToBusStand(){
+    print('Bus Stand');
+  }
+
+  void _movingViaBus(){
+    print('Moving Via Bus');
+  }
+}
